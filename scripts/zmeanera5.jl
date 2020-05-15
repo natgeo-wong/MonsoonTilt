7a7ecf37dea15ee0aa9a4f4c07f82af608b5fdcc
natgeo-wong <natgeo.wong@outlook.com>
2020-05-15 00:55:41 -0400

    Forgot to add src functions

--- scripts/zmeanera5.jl
+++ scripts/zmeanera5.jl
@@ -2,6 +2,7 @@ using DrWatson
 @quickactivate "MonsoonTilt"
 
 global_logger(ConsoleLogger(stdout,Logging.Info))
+include(srcdir("compiled.jl"))
 
 era5matrix(modID="msfc",parID="prcp_tot")
 era5matrix(modID="dpre",parID="t_air",pre=1000)
