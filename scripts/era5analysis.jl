eae393c09bf8eb305f602554356e2fabd3645f7d
Nathanael Wong <natgeo.wong@outlook.com>
2020-05-14 22:56:45 -0400

    Add ASM GeoRegion for this project

--- scripts/era5analysis.jl
+++ scripts/era5analysis.jl
@@ -1,11 +1,12 @@
 using DrWatson
-@quickactivate "PiPWV"
+@quickactivate "MonsoonTilt"
 using ClimateERA
 
 global_logger(ConsoleLogger(stdout,Logging.Info))
 
 prjpath = "/n/holyscratch01/kuang_lab/nwong/MonsoonTilt/ecmwf/";
 init,eroot = erastartup(aID=1,dID=1,path=prjpath); t = [1980,2019];
+gregioninfoadd(srcdir("addGeoRegions.txt"));
 
 eranalysis(init,eroot,modID="msfc",parID="prcp_tot",regID="ASM",timeID=t,plvls="sfc");
 eranalysis(init,eroot,modID="dpre",parID="t_air",regID="ASM",timeID=t,plvls=1000);
--- scripts/era5download.jl
+++ scripts/era5download.jl
@@ -1,11 +1,12 @@
 using DrWatson
-@quickactivate "PiPWV"
+@quickactivate "MonsoonTilt"
 using ClimateERA
 
 global_logger(ConsoleLogger(stdout,Logging.Info))
 
 prjpath = "/n/holyscratch01/kuang_lab/nwong/MonsoonTilt/ecmwf/";
 init,eroot = erastartup(aID=1,dID=1,path=prjpath);
+gregioninfoadd(srcdir("addGeoRegions.txt"));
 
 eradownload(init,eroot,modID="msfc",parID="prcp_tot",regID="ASM",timeID=[1980,2019])
 eradownload(init,eroot,modID="dpre",parID="t_air",regID="ASM",timeID=[1980,2019])
